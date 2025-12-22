9b8b19b03542a6612c9fe2552ac587feaca0d643
asrim10 <asrimmsuwall@gmail.com>
2025-12-22 10:22:14 +0545

    usermodel and type

---
+++ src/types/user.type.ts
@@ -0,0 +1,12 @@
+import z, { email } from "zod";
+
+export const UserSchema = z.object({
+  email: z.email().min(5),
+  password: z.string().min(8),
+  username: z.string().min(3).max(8),
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+  role: z.enum(["user", "admin"]).default("user"),
+});
+
+export type UserType = z.infer<typeof UserSchema>;
